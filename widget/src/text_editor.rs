d0a944205aac31a79cbf590cd2dcade3261c76c4
lufte <javier@lufte.net>
2024-09-18 23:32:50 -0300

    Set the text color determined by the style function
    Fixes: https://github.com/iced-rs/iced/issues/2557

--- widget/src/text_editor.rs
+++ widget/src/text_editor.rs
@@ -764,7 +764,7 @@ where
         tree: &widget::Tree,
         renderer: &mut Renderer,
         theme: &Theme,
-        defaults: &renderer::Style,
+        _defaults: &renderer::Style,
         layout: Layout<'_>,
         cursor: mouse::Cursor,
         _viewport: &Rectangle,
@@ -833,7 +833,7 @@ where
             renderer.fill_editor(
                 &internal.editor,
                 text_bounds.position(),
-                defaults.text_color,
+                style.value,
                 text_bounds,
             );
         }
